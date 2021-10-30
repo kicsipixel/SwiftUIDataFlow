1817fd78667a6bfe971585f301c0c38dff7eba63
Szabolcs Tóth <tsz@purzelbaum.hu>
2021-10-30 12:43:35 +0200

    .gitignore added and Combine project created.

---
+++ Combine/NickNickCombine/NickNickCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NickNickCombine
+//
+//  Created by Szabolcs Tóth on 2021. 10. 30..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Combine/NickNickCombine/NickNickCombine/NickNickCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NickNickCombineApp.swift
+//  NickNickCombine
+//
+//  Created by Szabolcs Tóth on 2021. 10. 30..
+//
+
+import SwiftUI
+
+@main
+struct NickNickCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
